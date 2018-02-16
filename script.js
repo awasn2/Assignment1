a06fc4dfc945244e59b01a3f1ebe8c8ff4ce9ce4
awasn2 <awasn2@gmail.com>
2018-02-16 16:22:47 +0530

    Ruchis initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function display() {
+  var x= document.getElementById('contact');
+    if(x.style.display==="none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+        
+    }//displays the contact details onclick of image
+
